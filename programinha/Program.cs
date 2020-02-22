fc30b29b9f128a5cc5d67b6fa7035040fb7a8403
RickBraz <riicardobraz@gmail.com>
2020-02-21 22:37:49 -0300

    botei um programa em csharp

---
+++ programinha/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace programinha
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("o problema nao e o frio e calor!");
+            Console.ReadKey();
+            Console.WriteLine("o problema e ser pobre ksksksksk");
+            Console.ReadKey();   
+        }
+    }
+}
